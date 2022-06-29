d6e365fdef138150249d9e918c7d06d6ef19f9c3
l0gOut <daniil.petrunin2013@yandex.ru>
2022-06-29 19:35:27 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [nameCity, setNameCity] = useState("");
+  const [coord, setCoord] = useState("");
+
+  async function findCity(e) {
+    e.preventDefault();
+    const weather = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          q: nameCity,
+          appid: "94f0682e42c7aabe667957f6e85d502b",
+          units: "metric",
+          lang: "ru",
+        },
+      }
+    );
+
+    console.log(weather.data);
+  }
+
+  function geo() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setCoord(
+          `Широта ${position.coords.latitude}, Долгота ${position.coords.longitude}`
+        );
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={geo}>Геолокация</button>
+      <form onSubmit={e => findCity(e)}>
+        <input
+          type="text"
+          placeholder="Введите город"
+          value={nameCity}
+          onChange={e => setNameCity(e.target.value)}
+        />
+        <button type="submit">Найти</button>
+      </form>
+      <h1>{coord}</h1>
     </div>
   );
 }
